a713d4c43a9862f1076136ca902b2313e10a22bd
Saif Rehman <stunner.ey@gmail.com>
2025-02-04 17:49:38 +0500

    Update Build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-ModelPreviewWebGL-0.1";
 const contentToCache = [
-    "Build/Build.loader.js",
-    "Build/Build.framework.js",
-    "Build/Build.data",
-    "Build/Build.wasm",
+    "Build/New Build WENG.loader.js",
+    "Build/New Build WENG.framework.js",
+    "Build/New Build WENG.data",
+    "Build/New Build WENG.wasm",
     "TemplateData/style.css"
 
 ];
